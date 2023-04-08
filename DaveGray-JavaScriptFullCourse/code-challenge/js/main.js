149fa9c1a9b685a81a6b06e37e7aa3fdfcd3b276
Isaac Lal <isaaclal124@gmail.com>
2023-04-08 18:26:25 -0400

    40:05 - Learning if statements"

---
+++ DaveGray-JavaScriptFullCourse/code-challenge/js/main.js
@@ -0,0 +1,28 @@
+// Your first Code Challenge
+
+// Write code that will return a random letter from your name
+
+const myName = 'Isaac';
+const anotherName = 'Johnathan';
+
+console.log(typeof myName);
+
+console.log(myName.charAt());
+
+console.log(Math.floor(Math.random() * myName.length + 1));
+
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+console.log(anotherName.charAt(Math.floor(Math.random() * anotherName.length)));
+
+// I FIGURED OUT LENGTH BY MYSELF WOOOOOOOOOOO
