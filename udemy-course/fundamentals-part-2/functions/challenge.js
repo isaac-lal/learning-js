9d584530145b45883008972bd3bddb6819dc0f17
Isaac Lal <isaaclal124@gmail.com>
2023-05-11 14:29:29 -0400

    Learned about functions

---
+++ udemy-course/fundamentals-part-2/functions/challenge.js
@@ -0,0 +1,14 @@
+/*
+1. Write a function called "describeCountry" which takes three parameters: "country", "population", and "capitalCity". Based on this input, the function returns a string with this format: "Finland has 6 million people and its capital city is Helsinki"
+2. Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console
+*/
+
+function describeCountry(country, population, capitalCity) {
+  const countryDescription = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+
+  return countryDescription;
+}
+
+console.log(describeCountry("United States", 330, "Washington D.C."));
+console.log(describeCountry("Italy", 59.11, "Rome"));
+console.log(describeCountry("Germany", 83.2, "Berlin"));
--- udemy-course/fundamentals-part-2/functions/script.js
+++ udemy-course/fundamentals-part-2/functions/script.js
@@ -13,10 +13,24 @@ logger();
 logger();
 logger();
 logger();
-logger();  // omg im learning wow say hi 
+logger(); // omg im learning wow say hi
 logger(); // im also testing out the smooth type animation and wow it's nice
 
-function fruitProcessor(apples, bananas) { // function names should always be descriptive and the parameters are variable names for the function
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oranges}`;
+function fruitProcessor(apples, bananas) {
+  // function names should always be descriptive and the parameters are variable names for the function
+  console.log(apples, bananas);
+  const juice = `Juice with ${apples} apples and ${bananas} bananas`;
+
+  return juice;
 }
+
+const appleJuice = fruitProcessor(5, 0); // the numbers display in order so 5 apples and 0 oranges
+console.log(appleJuice); // the result of the function is assigned to appleJuice
+console.log(fruitProcessor(5, 0));
+
+const appleBananaJuice = fruitProcessor(2, 4); // the values of apples and oranges can be accessed at any point
+console.log(appleBananaJuice); // DRY: Don't Repeat Yourself
+
+Number('23'); // Number is a built in function that takes 23 as a parameter
+
+const num = Number("23");
