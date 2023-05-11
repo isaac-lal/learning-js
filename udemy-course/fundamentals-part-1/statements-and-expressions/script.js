9173855ff183103965f61166264d7dd7a70743f1
Isaac Lal <isaaclal124@gmail.com>
2023-05-11 12:44:26 -0400

    switch statement coding challenge

---
+++ udemy-course/fundamentals-part-1/statements-and-expressions/script.js
@@ -0,0 +1,11 @@
+// 3 + 4 is an expression and 1991 is also one because it will product a value
+// true && false && !false will be an expression because it produces a value
+// statements and declarations are like actions so for example
+
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+console.log(`I'm ${2037 - 1991} years old`); // cannot insert if statements inside the expression
+const me = "Isaac";
+console.log(`I'm ${2037 - 1991} years old ${me}`);
