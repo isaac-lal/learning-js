80cccf3b475bbd83a81b55dad1ca09f768d36080
Isaac Lal <isaaclal124@gmail.com>
2023-05-11 13:10:04 -0400

    Completed fourth coding challenge

---
+++ udemy-course/fundamentals-part-1/fourth-coding-challenge/script.js
@@ -0,0 +1,20 @@
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement. (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). 
+Example: "The bill was 275, the tip was 41.25, and the total value 316.25"
+
+TEST DATA: Test for bill values 275, 40, and 430
+*/
+
+const tipValueOne = 0.15,
+  tipValueTwo = 0.2;
+const bill = 430;
+
+bill > 50 && bill < 300
+  ? (tip = bill * tipValueOne)
+  : (tip = bill * tipValueTwo);
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
